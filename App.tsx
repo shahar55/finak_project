006a86def43d8d78f045229e63d822371dea3281
ItamarGefen <88192967+ItamarGefen@users.noreply.github.com>
2023-03-29 20:59:09 +0300

    default react native app

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {Home} from './src/screens';
+
+function App(): JSX.Element {
+  return <Home />;
+}
+export default App;
