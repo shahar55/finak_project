2547ebc4bee187a23b028faecff141ed8139bd78
ItamarGefen <88192967+ItamarGefen@users.noreply.github.com>
2023-03-30 20:04:21 +0300

    added stack navigator

--- App.tsx
+++ App.tsx
@@ -7,10 +7,18 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Home} from './src/screens';
 
+const Stack = createNativeStackNavigator();
 function App(): JSX.Element {
-  return <NavigationContainer>{<Home />}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 export default App;
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ beginner musicians helper
 <h2>npm libraries used in this project:</h2>
 <ul>
     <li>npm install @react-navigation/native react-native-screens react-native-safe-area-context</li>
+    <li>npm install @react-navigation/native-stack</li>
     <li>npm install @spotify/basic-pitch</li>
 </ul>
