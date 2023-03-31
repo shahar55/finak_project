be46f6dff76a05aa7b63e248cc6e0516ea92a5ba
ItamarGefen <88192967+ItamarGefen@users.noreply.github.com>
2023-03-31 06:21:12 +0300

    added record & play functionality

--- src/screens/Recording.tsx
+++ src/screens/Recording.tsx
@@ -1,12 +1,79 @@
 import React from 'react';
-import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, View, StyleSheet, Text, Button} from 'react-native';
+
+import {Audio} from 'expo-av';
 
 function Recording(): JSX.Element {
-  console.log('hello');
+  const [recording, setRecording] = React.useState<Audio.Recording>();
+  const [uri, setUri] = React.useState<string | null>();
+  const [sound, setSound] = React.useState<Audio.Sound>();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const {recording: _recording} = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY,
+      );
+      setRecording(_recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording?.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const _uri = recording?.getURI();
+    console.log('Recording stopped and stored at', _uri);
+    setUri(_uri);
+  }
+
+  async function playSound() {
+    if (uri) {
+      console.log('Loading Sound');
+      console.log(uri);
+      const {sound: _sound} = await Audio.Sound.createAsync({uri: uri});
+      setSound(_sound);
+      console.log(_sound);
+      console.log('Playing Sound');
+      await _sound?.playAsync();
+    }
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <SafeAreaView>
       <View style={styles.Container}>
         <Text style={styles.Text}>RECORDING SCREEN</Text>
+      </View>
+      <View style={styles.Container}>
+        <Button title="Start" onPress={startRecording} />
+      </View>
+      <View style={styles.Container}>
+        <Button title="Stop" onPress={stopRecording} />
+      </View>
+      <View style={styles.Container}>
+        <Button title="Play" onPress={playSound} />
       </View>
     </SafeAreaView>
   );
